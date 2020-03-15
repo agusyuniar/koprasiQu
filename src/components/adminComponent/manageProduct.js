f3386891f605fb31e72c437a404c572a6494b186
agusyuniar <uu.niar@gmail.com>
2020-03-16 00:04:28 +0700

    rapihin admin page

---
+++ src/components/adminComponent/manageProduct.js
@@ -0,0 +1,233 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import CheckCircleOutlineRoundedIcon from '@material-ui/icons/CheckCircleOutlineRounded';
+import ErrorOutlineOutlinedIcon from '@material-ui/icons/ErrorOutlineOutlined';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { API_URL_1 } from "../../helpers/apiurl";
+import { getUserOrtu, inputEditText, submitEdit } from "../../redux/action";
+
+
+
+class ManageMurid extends Component {
+    state = {
+        selectedID: 0
+    }
+
+    componentDidMount() {
+        this.getInitialDataOrtu()
+    }
+    getInitialDataOrtu = () => {
+        this.props.getUserOrtu()
+    }
+
+    renderData = () => {
+        return this.props.getData.dataParent.map((data, index) => {
+            //define role
+            console.log(data.alamat);
+            
+            if (data.role_id == 1) {
+                var role_idrender = 'SuperAdmin'
+            }
+            else if (data.role_id == 2) {
+                var role_idrender = 'AdminToko'
+            }
+            else if (data.role_id == 3) {
+                var role_idrender = 'AdminParent'
+            }
+            else if (data.role_id == 4) {
+                var role_idrender = 'UserStudent'
+            }
+
+            if (data.verified) {
+                var verifiedrender = <CheckCircleOutlineRoundedIcon style={{ color: 'green' }} />
+            } else {
+                var verifiedrender = <ErrorOutlineOutlinedIcon style={{ color: 'red' }} />
+            }
+            var anak = JSON.parse(data.anak)
+            console.log(anak);
+
+            if (this.state.selectedID == data.id) {
+                return (
+                    <tr>
+                        <td className='border p-1' scope="row" >{index + 1}</td>
+                        <td className='border p-1'>
+                            <input
+                                // onChange={(val) => this.props.inputEditText('id', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.id} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.verified}
+                                onChange={(val) => this.props.inputEditText('verified', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.verified} />
+                        </td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.username}
+                                onChange={(val) => this.props.inputEditText('username', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.username} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.firstname}
+                                onChange={(val) => this.props.inputEditText('firstname', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.firstname} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.lastname}
+                                onChange={(val) => this.props.inputEditText('lastname', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.lastname} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.alamat}
+                                onChange={(val) => this.props.inputEditText('alamat', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.alamat} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.email}
+                                onChange={(val) => this.props.inputEditText('email', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.email} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.role_id}
+                                onChange={(val) => this.props.inputEditText('role_id', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.role_id} /></td>
+                        <td className='border p-1'>
+                            <input
+                                value={this.props.adminEdit.profil_img}
+                                onChange={(val) => this.props.inputEditText('profil_img', val.target.value)}
+                                type="text"
+                                className="form-control" placeholder={data.profil_img} /></td>
+                        <td className='border p-1'><input
+                            // value={this.props.regisForm.confPassword}
+                            // onChange={(val) => this.props.inputText('username', val.target.value)}
+                            type="text"
+                            className="form-control" placeholder='Edit di Manage Siswa' disabled /></td>
+                        <td className='border p-1'>
+                            <button onClick={() => this.setState({ selectedID: 0 })} style={{ backgroundColor: 'red' }}>Cancel</button>
+                            <button onClick={this.onBtnEditSavePress} style={{ backgroundColor: 'green' }}>Save</button>
+                        </td>
+                    </tr>
+                )
+            }
+            return (
+                <tr>
+                    <td className='border p-1' scope="row">{index + 1}</td>
+                    <td className='border p-1'>{data.id}</td>
+                    <td className='border p-1'>{verifiedrender}</td>
+                    <td className='border p-1'>{data.username}</td>
+                    <td className='border p-1'>{data.firstname}</td>
+                    <td className='border p-1'>{data.lastname}</td>
+                    <td className='border p-1'>{data.alamat}</td>
+                    <td className='border p-1'>{data.email}</td>
+                    <td className='border p-1'>{data.role_id}</td>
+                    <td className='border p-1'><img src={API_URL_1 + data.profil_img} height='100' /></td>
+                    <td className='border p-1'>{
+                        anak.map((list) => {
+                            if (!list.nim) {
+                                return 'Belum terhubung'
+                            }
+                            return (
+                                <div className='border p-1'>
+                                    <ExpansionPanel >
+                                        <ExpansionPanelSummary
+                                            expandIcon={<ExpandMoreIcon fontSize='small' />}
+                                            // aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                        >
+                                            <p className='p'>{list.nama}</p>
+                                        </ExpansionPanelSummary>
+                                        <ExpansionPanelDetails>
+                                            <table className=''>
+                                                <tr className='border'>
+                                                    <td className='border'>NIM</td>
+                                                    <td className='border'>Alamat</td>
+                                                    <td className='border'>Saldo</td>
+                                                    <td className='border'>Avatar</td>
+                                                </tr>
+                                                <tr className='border'>
+                                                    <td className='border'>{list.nim}</td>
+                                                    <td className='border'>{list.alamat}</td>
+                                                    <td className='border'>Rp {list.saldo.toLocaleString()}</td>
+                                                    <td className='border'><img src={API_URL_1 + list.profil_img} width='80px' /></td>
+                                                </tr>
+                                            </table>
+                                        </ExpansionPanelDetails>
+                                    </ExpansionPanel>
+                                </div>
+
+                            )
+                        })
+                    }
+                    </td>
+                    <td className='border p-1'>
+                        <button onClick={() => this.setState({ selectedID: data.id })}>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    onBtnEditSavePress = () => {
+        var { verified, username, firstname, lastname, alamat, email, role_id, profil_img } = this.props.adminEdit
+        // if(verified||username||firstname||lastname||alamat||email||role_id||profil_img)
+        this.props.adminEdit.id = this.state.selectedID
+        this.props.submitEdit(this.props.adminEdit)
+        this.state.selectedID = 0
+        this.getInitialDataOrtu()
+        this.renderData()
+    }
+
+    showMurid = (id) => {
+        console.log(id);
+        return <a>{id}</a>
+    }
+
+
+    render() {
+
+        console.log(this.props.getData.dataParent[3]);
+
+        return (
+            <div>
+                <div className='h4 '>Data Product Koperasi</div>
+                <table className="shadow rounded p-5" >
+                    <thead className='border p-1'>
+                        <tr>
+                            <th className='border' scope="col">No</th>
+                            <th className='border' scope="col">ID</th>
+                            <th className='border' scope="col">Verified</th>
+                            <th className='border' scope="col">Username</th>
+                            <th className='border' scope="col">Nama Depan</th>
+                            <th className='border' scope="col">Nama Belakang</th>
+                            <th className='border' scope="col">Alamat</th>
+                            <th className='border' scope="col">Email</th>
+                            <th className='border' scope="col">Role</th>
+                            <th className='border' scope="col">Avatar</th>
+                            <th className='border' scope="col">Anak</th>
+                            <th className='border' scope="col">...</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderData()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+const sambungin = ({ getData, adminEdit }) => {
+    return { getData, adminEdit }
+}
+export default connect(sambungin, { getUserOrtu, inputEditText, submitEdit })(ManageMurid);
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,51 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import DataOrtu from '../components/adminComponent/manageOrtu';
+import DataProduct from '../components/adminComponent/manageProduct';
 
 class LoginPage extends Component {
-    state = { }
-
-    toogleShow(){
-        if(this.state.showMurid){
-            this.state.showParent=false
-            this.state.showProduct=false
-        }
-        else if(this.state.showParent){
-            this.state.showMurid=false
-            this.state.showProduct=false
-        }
+    state = {
+        activeTab: 0
     }
 
     render() {
+
         console.log(this.state);
-        
+
         return (
             <div className=' m-auto p-5' style={{ backgroundImage: 'linear-gradient(to top, #dfe9f3 0%, white 100%)' }}>
                 <div className=''>
-                        <h3 className='pl-4 align-left'><strong>Halo, Admin</strong></h3>
+                    <h3 className='pl-4 align-left'><strong>Halo, Admin</strong></h3>
                     <div className='container-flex text-center'>
                         <div className='p-3 rounded' style={{ backgroundColor: 'rgba(255, 255, 255, .8)' }}>
                             <div className=''>
                                 <div className='border rounded p-3 m-2'>
-                                    <ButtonGroup
+                                    {/* <ButtonGroup
                                         orientation="horizontal"
                                         color="light"
                                         size='small'
                                         variant='contained'
                                     >
-                                        <Button style={{ textTransform: 'none', backgroundColor: '#cfd186' }}>Manage Siswa</Button>
-                                        <Button onClick={()=>this.setState({showParent:!this.state.showParent})} style={{ textTransform: 'none', backgroundColor: '#cfd186' }}>Manage Orangtua</Button>
-                                        <Button style={{ textTransform: 'none', backgroundColor: '#cfd186' }}>Manage Product</Button>
-                                        <Button style={{ textTransform: 'none', backgroundColor: '#cfd186' }}>Manage siswa</Button>
-                                    </ButtonGroup>
+                                        <Button onClick={() => this.setState({ showParent: !this.state.showParent })} style={{ textTransform: 'none', backgroundColor: '#cfd186' }}>Manage Orangtua</Button>
+                                        <Button onClick={() => this.setState({ showProduct: !this.state.showProduct })} style={{ textTransform: 'none', backgroundColor: '#cfd186' }}>Manage Product</Button>
+                                    </ButtonGroup> */}
+                                <div>
+                                    <Nav tabs>
+                                        <NavItem className='text-center'>
+                                            <NavLink
+                                                className={this.state.activeTab == 1 ? 'active btn-warning' : null}
+                                                onClick={() => this.setState({ activeTab: '1' })}
+                                            >
+                                                Manage Orang Tua
+                                            </NavLink>
+                                        </NavItem>  
+                                        <NavItem>
+                                            <NavLink
+                                                className={this.state.activeTab == 2 ? 'active btn-warning' : null}
+                                                onClick={() => this.setState({ activeTab: '2' })}
+                                            >
+                                                Manage Produk
+                                            </NavLink>
+                                        </NavItem>
+                                    </Nav>
+                                    <TabContent activeTab={this.state.activeTab}>
+                                        <TabPane tabId="1">
+                                            <Row>
+                                                <Col>
+                                                    <DataOrtu />
+                                                </Col>
+                                            </Row>
+                                        </TabPane>
+                                        <TabPane tabId="2">
+                                            <Row>
+                                                <Col>
+                                                    <DataProduct />
+                                                </Col>
+                                            </Row>
+                                        </TabPane>
+                                    </TabContent>
                                 </div>
-                                <div className='border rounded p-3 m-2'>
-                                    {/* render Parent */}
-                                    {this.state.showParent?<DataOrtu/>:null}
                                 </div>
+
                             </div>
                         </div>
                     </div>
@@ -54,6 +79,7 @@ class LoginPage extends Component {
         );
     }
 }
+
 
 const sambungin = ({ adminEdit }) => {
     return { adminEdit }
